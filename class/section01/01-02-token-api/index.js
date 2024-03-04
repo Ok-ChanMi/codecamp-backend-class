abf613e8b802fb1bf1359dcf6ca7d7aca16b006f
Ok-ChanMi <chan04152@naver.com>
2024-03-04 12:01:46 +0900

    2024.03.02

---
+++ class/section01/01-02-token-api/index.js
@@ -0,0 +1,17 @@
+function createTokenOfPhone() {
+    // 1. 휴대폰번호 자릿수 맞는지 확인하기(10~11자리)
+    if(qqq.length >=10) {
+        if(qqq.length <= 11) {
+            
+        } else {
+            console.log("에러 발생!!! 핸드폰 번호를 제대로 입력해 주세요")
+        }
+    } else {
+        console.log("에러 발생 !!! 핸드폰 번호를 제대로 입력해주세요")
+    }
+
+    // 2. 핸드폰 토근 6자리 만들기
+
+    // 3. 핸드폰번호에 토큰 전송하기
+}
+createTokenOfPhone()
